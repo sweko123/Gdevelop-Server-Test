86f332e503c60c3c68c9eebde92b7499f7725a92
sweko123 <swekogamer@gmail.com>
2025-09-27 09:29:28 -1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("playerMove", (data) => {
+    socket.broadcast.emit("playerMove", { id: socket.id, ...data });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
